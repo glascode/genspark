902d0724e5072410e9bda7f5721f6396ff35f304
Anthony G <anthonycom.garcia04@gmail.com>
2022-05-20 16:03:05 -0700

    boarding pass GUI
    Realized the design would be a pain to execute sequentially, streamlined the stages. This did result in more files, however the it's much easier to work with

---
+++ BoardingPassGUI/src/main/java/com/example/boardingpassgui/Controllers/GUIController.java
@@ -0,0 +1,23 @@
+package com.example.boardingpassgui.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GUIController {
+   private Stage stage;
+   private Scene scene;
+   private Parent root;
+
+   public void switchToScene2(ActionEvent e) throws IOException {
+
+
+   }
+}
