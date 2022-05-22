a3f49988a7924789ddf92a6a8731d7c52c476da1
Anthony G <anthonycom.garcia04@gmail.com>
2022-05-22 15:10:00 -0700

    linked the stages, save back/decline options

---
+++ BoardingPassGUI/HelloApplication.java
@@ -0,0 +1,33 @@
+package com.example.boardingpassgui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("start-view.fxml"));
+        Scene scene = new Scene(root, Color.DARKGRAY);
+        stage.setTitle("Boarding Pass Program");
+
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+
+        launch();
+    }
+}
